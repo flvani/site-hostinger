683f53c041faa66def4dea4e2d51f2dab89aee14
Flavio Vani <flavio.vani@camara.leg.br>
2022-03-04 14:57:23 -0300

    v6.12:Idioma russo

--- site/site_6.12.js
+++ site/site_6.12.js
@@ -119,8 +119,8 @@ SITE.LoadProperties = function() {
         salvar = true;
         
     }
-    
-    if( !SITE.properties.known_languages ) {
+   
+    if( !SITE.properties.known_languages || !SITE.properties.known_languages.ru_RU ) {
 
         SITE.properties.known_languages = {
              de_DE: { file: 'languages/de_DE.lang', image: "images/de_DE.png", name: 'Deustch' } 
@@ -128,6 +128,7 @@ SITE.LoadProperties = function() {
             ,es_ES: { file: 'languages/es_ES.lang', image: "images/es_ES.png", name: 'Español' } 
             ,fr_FR: { file: 'languages/fr_FR.lang', image: "images/fr_FR.png", name: 'Français' } 
             ,it_IT: { file: 'languages/it_IT.lang', image: "images/it_IT.png", name: 'Italiano' } 
+            ,ru_RU: { file: 'languages/ru_RU.lang', image: "images/ru_RU.png", name: 'Русский язык' } 
             ,pt_BR: { file: 'languages/pt_BR.lang', image: "images/pt_BR.png", name: 'Português do Brasil' } 
         };
 
@@ -241,6 +242,7 @@ SITE.ResetProperties = function() {
         ,es_ES: { file: 'languages/es_ES.lang', image: "images/es_ES.png", name: 'Español' } 
         ,fr_FR: { file: 'languages/fr_FR.lang', image: "images/fr_FR.png", name: 'Français' } 
         ,it_IT: { file: 'languages/it_IT.lang', image: "images/it_IT.png", name: 'Italiano' } 
+        ,ru_RU: { file: 'languages/ru_RU.lang', image: "images/ru_RU.png", name: 'Русский язык' } 
         ,pt_BR: { file: 'languages/pt_BR.lang', image: "images/pt_BR.png", name: 'Português do Brasil' } 
     };
     
@@ -887,14 +889,14 @@ SITE.Mapa = function( interfaceParams, tabParams, playerParams ) {
                     'Símbolos de Repetição|JUMPS',
                     'Estúdio ABCX|ESTUDIO',
                     'Formato ABCX|ABCX',
-                    'Vídeo Tutoriais|TUTORIAL',
                     'Sobre|ABOUT'
                 ]}
         ]);
+//        'Vídeo Tutoriais|TUTORIAL',
+    //this.menu.disableSubItem( 'menuInformacoes', 'TUTORIAL' );
         
-    this.menu.disableSubItem( 'menuInformacoes', 'ESTUDIO' );
-    this.menu.disableSubItem( 'menuInformacoes', 'ABCX' );
-    this.menu.disableSubItem( 'menuInformacoes', 'TUTORIAL' );
+    //this.menu.disableSubItem( 'menuInformacoes', 'ESTUDIO' );
+    //this.menu.disableSubItem( 'menuInformacoes', 'ABCX' );
         
     this.accordionSelector = new ABCXJS.edit.AccordionSelector( 
             'menuGaitas', this.menu, 
@@ -2154,6 +2156,7 @@ SITE.Mapa.prototype.settingsCallback = function (action, elem) {
         case 'fr_FR':
         case 'it_IT':
         case 'pt_BR':
+        case 'ru_RU':
             this.settings.lang = action;
             this.settings.menu.setSubMenuTitle( 'menuIdiomas', this.settings.menu.selectItem( 'menuIdiomas', action ));
             break;
@@ -2404,56 +2407,74 @@ SITE.Mapa.prototype.showHelp = function ( title, subTitle, url, options ) {
     
     this.helpWindow.dataDiv.innerHTML = '<object data="'+url+'" type="text/html" ></object>';
     this.iframe = this.helpWindow.dataDiv.getElementsByTagName("object")[0];
-    
-    
     this.iframe.style.width = options.width+"px";
     this.iframe.style.height = (options.height?options.height:400)+"px";
-    //that.helpWindow.topDiv.style.opacity = "0";
-    //that.helpWindow.topDiv.style.opacity = "1";
-    that.helpWindow.setVisible(true);
+    this.helpWindow.setVisible(true);
             
     var loader = this.startLoader( "About", this.helpWindow.dataDiv );
 
+    that.helpWindow.dataDiv.style.opacity = "1";
+    that.helpWindow.dataDiv.style.overflow = "hidden";
+
     loader.start(  function() { 
-        if( options.height ) {
-            that.iframe.addEventListener("load", function () { 
-                that.helpWindow.topDiv.style.opacity = "1";
-                that.iframe.style.height = options.height+"px";
-                
-                var header = this.contentDocument.getElementById('helpHeader');
-                var frm = this.contentDocument.getElementById('helpFrame');
-                var container = this.contentDocument.getElementById('helpContainer');
-                if( header ) header.style.display = 'none';
-                if( frm ) frm.style.overflow = 'hidden';
-                if( container ) {
-                    container.style.top = '0';
-                    container.style.height = (options.height-18)+"px";;
-                    container.style.overflow = 'hidden';
-                    container.style.border = '1px solid rgba(255, 153, 34, 0.2)';
-                    var v = new PerfectScrollbar( container, {
-                        handlers: ['click-rail', 'drag-thumb', 'keyboard', 'wheel', 'touch'],
-                        wheelSpeed: 1,
-                        wheelPropagation: false,
-                        suppressScrollX: false,
-                        minScrollbarLength: 100,
-                        swipeEasing: true,
-                        scrollingThreshold: 500
-                    });
-                }
-                
-                loader.stop();
-            });    
-        } else { // auto determina a altura
+
+        if( options.height === undefined ) {// auto determina a altura
+
             that.iframe.addEventListener("load", function () { 
                 this.contentDocument.body.style.overflow ="hidden";
                 var info = this.contentDocument.getElementById('siteVerI');
                 if( info ) info.innerHTML=SITE.siteVersion;
-                that.iframe.style.height = this.contentDocument.body.clientHeight+"px";
-                //that.helpWindow.topDiv.style.opacity = "1";
-                that.helpWindow.dataDiv.style.overflow = "hidden";
+                this.style.height = this.contentDocument.body.clientHeight+"px";
+                that.helpWindow.dataDiv.style.opacity = "1";
                 loader.stop();
             });
+
+        } else {
+            
+            var myInterval = window.setInterval( function checkFrameLoaded() {
+                var header = that.iframe.contentDocument.getElementById('helpHeader');
+                if( header ){
+                    clearInterval( myInterval )
+                    that.helpWindow.dataDiv.style.opacity = "0";
+                    /* se pensar em implementar janela full para o help, eis o começo
+                        that.helpWindow.move(0,80);
+                        that.helpWindow.setSize( "calc( 100% - 10px)", "calc( 100% - 90px)" );
+                        options.height = that.helpWindow.topDiv.clientHeight;
+                        that.iframe.style.height = options.height + 'px';
+                    */
+                    var header = that.iframe.contentDocument.getElementById('helpHeader');
+                    var frm = that.iframe.contentDocument.getElementById('helpFrame');
+                    var container = that.iframe.contentDocument.getElementById('helpContainer');
+                    if( header ) header.style.display = 'none';
+                    if( frm ) frm.style.overflow = 'hidden';
+        
+                    that.iframe.style.height = options.height+"px";
+        
+                    if( container ) {
+                        container.style.top = '0';
+                        container.style.height = (options.height-18)+"px";;
+                        container.style.overflow = 'hidden';
+                        container.style.border = '1px solid rgba(255, 153, 34, 0.2)';
+                        var v = new PerfectScrollbar( container, {
+                            handlers: ['click-rail', 'drag-thumb', 'keyboard', 'wheel', 'touch'],
+                            wheelSpeed: 1,
+                            wheelPropagation: false,
+                            suppressScrollX: false,
+                            minScrollbarLength: 100,
+                            swipeEasing: true,
+                            scrollingThreshold: 500
+                        });
+                    }
+        
+                    that.helpWindow.dataDiv.style.opacity = "1";
+                    loader.stop();
+                }
+                
+            }, 100);
+
+            //window.setTimeout( function() {}, 100);
         }
+
     }, '<br/>&#160;&#160;&#160;'+SITE.translator.getResource('wait')+'<br/><br/>' );
 };
 
@@ -2766,7 +2787,7 @@ SITE.Estudio = function (mapa, interfaceParams, playerParams) {
 
     this.slider = new DRAGGABLE.ui.Slider( this.tempoButton,
         {
-            min: 25, max: 200, start:100, step:25, speed:100, color: 'white', bgcolor:'red', size:{w:150, h:23, tw:48},
+            min: 25, max: 200, start:100, step:25, speed:100, color: 'white', bgcolor:'red', size:{w:115, h:23, tw:48},
             callback: function(v) { that.midiPlayer.setAndamento(v); } 
         } 
     );

4cc124bc7b4798bbc2491b68be82a5f6fb265a5c
Flávio Vani <flavio.vani@gmail.com>
2024-01-12 16:32:38 -0300

    remover abcx individuais

--- songs/check.videos.sh
+++
@@ -1,1 +0,0 @@
-grep X:$1 $( grep "^F:" *.abcx | awk -F ":" '{print $1}' ) |  awk -F":" '{print $1}' | sort -uk1 
--- songs/modify.videos.sh
+++
@@ -1,5 +0,0 @@
-for a in $(grep X:$1 $( grep "^F:" *.abcx | awk -F ":" '{print $1}' ) |  awk -F":" '{print $1}' | sort -uk1 )
-do
- cp $a $a.original
- sed 's/^F:/\%F:/g' $a.original > $a
-done
